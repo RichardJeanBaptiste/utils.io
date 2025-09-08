e4bd0819014fdfcf4d3d772417af40029bde8be9
Richard Jean-Baptiste <Richinbk1@gmail.com>
2025-09-08 19:33:16 -0400

    feat: added download playlist response to new route

--- bg_tasks.py
+++ bg_tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import yt_dlp
+import uuid
 
 celery_app = Celery(
     "worker",
@@ -9,9 +10,12 @@ celery_app = Celery(
 
 @celery_app.task
 def download_playlist(url: str):
+
+    folder_id = str(uuid.uuid4())
+
     ydl_opts = {
         "format": "bestaudio/best",
-        "outtmpl": "%(playlist_index)s - %(title)s.%(ext)s",
+        "outtmpl": "temp/" + folder_id + "/%(playlist_index)s - %(title)s.%(ext)s",
             "postprocessors": [{
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "mp3",
@@ -22,11 +26,19 @@ def download_playlist(url: str):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-    # try:
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except Exception as e:
+        error_msg = str(e)
+        if "certificate verify failed: unable to get local issuer certificate" in error_msg:
+            print(error_msg)
+            return {"ERROR": "Certificate verification failed. Please check your SSL certificates."}
+        elif isinstance(e, yt_dlp.utils.DownloadError):
+            print(error_msg)
+            return {"ERROR": "%(x)s is not a valid url"}
+        else:
+            print(error_msg)
+            return {"ERROR": "Something went wrong :("}
         
-    # except Exception as e:
-    #     if Exception == yt_dlp.utils.DownloadError:
-    #         return {"ERROR" : "%(x)s is not a valid url"}
-    #     else:
-    #         print(e)
-    #         return {"ERROR": "Something went wrong :("}
+    return {"success": "Playlists downloaded successfully"}
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from typing import Union
 from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from celery.result import AsyncResult
 from bg_tasks import download_playlist, celery_app
+import os
+import shutil
 
 
 app = FastAPI()
@@ -21,10 +24,35 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-def write_tasks():
-    with open('info.txt', 'w') as file:
-        file.write('This is a background task example.')
+@app.get("/status/{task_id}")
+def get_status(task_id: str):
+    task_result = AsyncResult(task_id, app=celery_app)
+    print(task_result)
+    return {
+        "task_id": task_id,
+        "status": task_result.status,
+        "result": task_result.result if task_result.ready() else None
+    }
 
+@app.get("/get_dlp/{folder_id}")
+def get_folder(folder_id: str):
+    '''
+        check if folder exists
+        return folder
+        auto delete folder after 10 mins
+    '''
+
+    folder_path = os.path.join("temp", folder_id)
+    zip_path = os.path.join("temp",f"{folder_id}.zip")
+
+    if(os.path.isdir(folder_path)):
+        
+        shutil.make_archive(base_name=zip_path.replace('.zip',''), format='zip', root_dir=folder_path)
+        
+        return FileResponse(zip_path, media_type="application/zip", filename=f"{folder_id}.zip")
+        #return {"folder_id": folder_id, "exists": True}
+    else:
+        return {"folder_id": folder_id, "exists": False}
 
 
 @app.post("/dlp")
@@ -33,18 +61,6 @@ async def get_playlist(item: Item, background_tasks: BackgroundTasks):
     # Check if private
     # Create Thread
 
-    #playlist_url = "https://www.youtube.com/playlist?list=PLPRWtKgY2MOuMQTifZgo3LWh51AWCImgA"
-
     task = download_playlist.delay(item.name)
     return {"task_id": task.id, "status": "submitted"}
-        
-    #return {"message": "Download started in the background."}
 
-@app.get("/status/{task_id}")
-def get_status(task_id: str):
-    task_result = AsyncResult(task_id, app=celery_app)
-    return {
-        "task_id": task_id,
-        "status": task_result.status,
-        "result": task_result.result if task_result.ready() else None
-    }
