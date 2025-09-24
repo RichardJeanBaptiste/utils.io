9caca6628e865540e4d5f188fa935c066ae582cb
Richard Jean-Baptiste <Richinbk1@gmail.com>
2025-09-24 10:46:06 -0400

    added login form to ui

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,9 +1,43 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import './HomePage.css'
+
 function HomePage() {
     return (
-        <div>
-            <p>Home Page</p>
+        <div className="root">
+            <Form className='login_form'>
+                <div style={{ width: '100%', height: '100%', position: 'relative'}}>
+                    <Form.Group className='email_form'>
+                        <Form.Label>Login:</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com"/>
+                    </Form.Group>
+
+                    <Form.Group className='pass_form'>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="passwprd" placeholder='*************'/>
+                    </Form.Group>
+
+                    <div className='login_icons'>
+                        <Button variant='light'>
+                            <FaGoogle/>
+                        </Button>
+
+                        <Button variant='light'>
+                            <FaFacebook/>
+                        </Button>
+                        
+                        <Button variant='light'>
+                            <FaGithub />
+                        </Button>
+                    </div>
+                </div> 
+            </Form>
         </div>
     )
 }
+
 
 export default HomePage;
