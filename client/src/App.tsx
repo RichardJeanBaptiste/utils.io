aaa2565cc998909a6d841593df78a3f991da7dfa
Richard Jean-Baptiste <Richinbk1@gmail.com>
2025-09-24 09:52:58 -0400

    preparing intial deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from 'react'
-import { Spinner } from 'react-bootstrap';
-import Test from './components/test';
+import { Spinner, Button, Form } from 'react-bootstrap';
 import './App.css'
-
-/**
- * Clear Selection
- * Style The UI
- */
-
 
 function App() {
 
@@ -17,7 +9,7 @@ function App() {
   const [folderId, setFolderId] = useState("");
   const [checkDownload, setCheckDownload] = useState(false);
   const [showDownloadLink, setShowDownloadLink] = useState(false);
-  const [showSpinner, setShowSpinner] = useState(false);
+  const [isDownloading, setIsDownloading] = useState(false)
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout | null = null;
@@ -27,18 +19,18 @@ function App() {
     // Check Download Status
     if (checkDownload) {
       intervalId = setInterval(() => {
-        console.log("Timed Response");
+        //console.log("Timed Response");
         fetch(`http://localhost:8000/status/${taskId}`,{
           method: 'GET',
           headers: myHeaders      
         })
         .then((response) => response.json())
         .then((data) => {
-          console.log(data.result.download_status);
+          //console.log(data.result.download_status);
           if(data.result.download_status){
             setShowDownloadLink(true);
             setCheckDownload(false);
-            setShowSpinner(false);
+            setIsDownloading(false);
           }
         })
       }, 10000);
@@ -54,10 +46,14 @@ function App() {
     setUrl(e.target.value);
   }
 
-  
-
   const handleSubmit = async () => {
-    setShowSpinner(true);
+
+    if(url == ""){
+      alert("Enter a link");
+      return;
+    }
+
+    setIsDownloading(true);
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json")
     await fetch('http://localhost:8000/dlp',{
@@ -65,55 +61,78 @@ function App() {
             body:JSON.stringify({"name": url}),
             headers: myHeaders,
             })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data)
-              setTaskId(data.task_id)
-              setFolderId(data.folder_id)
-              setCheckDownload(true);
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data)
+                setTaskId(data.task_id)
+                setFolderId(data.folder_id)
+                setCheckDownload(true);
             })
   }
 
   const resetPage = () => {
-      console.log("Reset");
       setUrl("");
       setTaskId("");
       setFolderId("");
       setCheckDownload(false);
-      setShowSpinner(false);
+      setIsDownloading(false);
       setShowDownloadLink(false);
   }
 
   const DownloadLink = () => {
-    if(showSpinner) {
+    if(isDownloading) {
       return (
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
+        <div style={Styles.input}>
+            <Spinner animation="border" role="status" variant='primary'>
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </div> 
       )
     } else if(showDownloadLink) {
         return (
-            <div>
-                <a style={{display : "block" }} href={`http://localhost:8000/get_dlp/${folderId}`}>Download Link</a>
-              <button onClick={resetPage}>Download Again?</button>
+            <div style={Styles.input}>
+                <Button variant="link" style={{display : "block" }} href={`http://localhost:8000/get_dlp/${folderId}`}>Download Link</Button>
+                <Button variant='primary' onClick={resetPage}>Download Again?</Button>
             </div>  
           )
     } else {
         return (
-          <div>
-              <input name='url' placeholder='Playlist link' value={url} onChange={handleUrl}/>
-              <button onClick={handleSubmit}>Submit</button>
+          <div style={Styles.input}>
+              <Form.Control name="url" type="text" placeholder='Playlist Link' value={url} onChange={handleUrl}></Form.Control>
+              <Button variant="outline-primary" size="sm" onClick={handleSubmit}>Submit</Button>
           </div> 
         )
     }
   }
 
   return (
-    <div>
+    <div style={Styles.root}>
       <DownloadLink/>
-      <p>Test Link : https://www.youtube.com/playlist?list=PLPRWtKgY2MOuMQTifZgo3LWh51AWCImgA</p>
+      <p>https://www.youtube.com/playlist?list=PLPRWtKgY2MOuMQTifZgo3LWh51AWCImgA</p>
     </div>
   )
+}
+
+
+const Styles: {root: React.CSSProperties, input: React.CSSProperties} = {
+  root: {
+    width: '100vw',
+    height: '100vh',
+    position: "relative",
+  },
+  input: {
+    position: 'absolute',
+    top: '10%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50vw',
+    height: '5vh',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '20px'
+  }
 }
 
 export default App
--- client/src/components/test.tsx
+++ client/src/components/test.tsx
@@ -1,42 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState, useEffect } from "react";
-import { Spinner } from 'react-bootstrap';
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { useState, useEffect } from "react";
+// import { Spinner } from 'react-bootstrap';
 
-const Test = () => {
-    const [downloadStart, setDownloadStart ] = useState(false);
-    const [isDownloading, setIsDownloading ] = useState(false);
-    const [downloadFinished, setDownloadFinished] = useState(false);
+// const Test = () => {
+//     const [downloadStart, setDownloadStart ] = useState(false);
+//     const [isDownloading, setIsDownloading ] = useState(false);
+//     const [downloadFinished, setDownloadFinished] = useState(false);
 
-    const startDownload = () => {
-        setIsDownloading(false);
-        setDownloadFinished(true);
-    }
+//     const startDownload = () => {
+//         setIsDownloading(false);
+//         setDownloadFinished(true);
+//     }
 
-    const stopDownload = () => {
-        setDownloadStart(false);
-        setDownloadFinished(false);
-        setIsDownloading(false);
-    }
+//     const stopDownload = () => {
+//         setDownloadStart(false);
+//         setDownloadFinished(false);
+//         setIsDownloading(false);
+//     }
 
-    if(isDownloading) {
-        return (
-            <div>
-                <p onClick={startDownload}>Loading.....</p>
-            </div>
-        )
-    } else if(downloadFinished) {
-        return (
-            <div>
-                <p onClick={stopDownload}>Download Finished....</p>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <p onClick={() => setIsDownloading(true)}>Ready To Download</p>
-            </div>
-        )
-    }
-}
+//     if(isDownloading) {
+//         return (
+//             <div>
+//                 <p onClick={startDownload}>Loading.....</p>
+//             </div>
+//         )
+//     } else if(downloadFinished) {
+//         return (
+//             <div>
+//                 <p onClick={stopDownload}>Download Finished....</p>
+//             </div>
+//         )
+//     } else {
+//         return (
+//             <div>
+//                 <p onClick={() => setIsDownloading(true)}>Ready To Download</p>
+//             </div>
+//         )
+//     }
+// }
 
-export default Test;
+// export default Test;
